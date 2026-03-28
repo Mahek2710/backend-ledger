b66accae256b46b1e99e32634a99562a11d604cc
Mahek2710 <2023.mahek.hingorani@ves.ac.in>
2026-03-29 01:19:35 +0530

    Implemented User model with password hashing and authentication methods

---
+++ src/models/user.model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required for creating an account"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+      unique: true,
+    },
+
+    name: {
+      type: String,
+      required: [true, "Name is required for creating an account"],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required for creating an account"],
+      minLength: [6, "Password must be at least 6 characters long"],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
